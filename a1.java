1468295a449189dc7322f5523052452d222212f9
Tim <timforby@gmail.com>
2017-02-01 16:21:11 -0500

    First commit with People you may know as base

---
+++ a1.java
@@ -0,0 +1,57 @@
+import java.io.IOException;
+import java.util.Arrays;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Lab2 {
+  public static class TokenizerMapper extends Mapper<Object, Text, IntWritable, IntWritable>{
+    private static IntWritable a = new IntWritable();
+    private static IntWritable b = new IntWritable();
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
+      System.out.println(value.toString());
+      String[] values = value.toString().split("\\s");
+      for(int i = 1 ; i < values.length; i++){
+        for(int j = i+1; j < values.length; j++){
+          a.set(Integer.parseInt(values[i]));
+          b.set(Integer.parseInt(values[j])); 
+          context.write(a, b);
+          context.write(b, a);
+        }
+      }
+    }
+  }
+  public static class IntSumReducer extends Reducer<IntWritable, IntWritable, IntWritable, Text>{
+    private static Text data = new Text();
+    public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
+      int[] nCommons = new int[]{0,0,0,0,0} ;
+      for (IntWritable val: values){
+        nCommons[val.get()-1]++;
+      }
+      String list = new String(key.get()+": ");
+      for (int i = 0; i < nCommons.length; i++){
+        if(nCommons[i]!=0)list += "("+(i+1)+","+nCommons[i]+")";
+      }
+      data.set(list);
+      context.write(key, data);
+    }
+  }
+  public static void main(String[] args) throws Exception{
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "lab2");
+    job.setJarByClass(Lab2.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(IntWritable.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0: 1);
+  }
+}
