fcd36bb0d80ae01863b70d35cfbffb4a2e2e1ba8
Tim <timforby@gmail.com>
2017-04-05 19:37:06 -0400

    clear

--- compile.sh
+++
@@ -1,3 +0,0 @@
-export CLASSPATH=${HADOOP_HOME}/share/hadoop/common/hadoop-common-2.7.3.jar:${HADOOP_HOME}/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.7.3.jar
-javac $1.java
-jar cvf $1.jar $1*.class 
